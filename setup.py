0f678589813b96fadae7cab86f010baa68f6ca66
mwelt <michael.welt@ubmw.de>
2015-05-06 16:36:01 +0200

    setup.py added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='opengeodb',
+      version='0.1',
+      description='Pythoninterface to OpenGeoDB',
+      author='Michael Welt',
+      author_email='',
+      url='https://github.com/mwelt/opengeodb/',
+      packages=['opengeodb'],
+      )
